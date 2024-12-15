0892e4d5ddb4ef786008bd4c97b3ce616df173cf
Adam Maas <adamjm1220@gmail.com>
2024-12-15 11:58:45 -0500

    Youtube API IAC 1

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+data "aws_ecr_image" "latest_image" {
+  repository_name = "youtube-containers"
+  image_tag       = "youtube-service-5"
+}
+
+data "aws_iam_role" "existing_lambda_role" {
+  name = "lambda_role_for_s3_access" #Name of the iam role that gives your lambda permissions
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "youtube-service-5"
+  role          = data.aws_iam_role.existing_lambda_role.arn
+
+  package_type = "Image"
+  image_uri    = "${data.aws_ecr_image.latest_image.image_uri}"
+
+  environment {
+    variables = {
+      LOGGING_BUCKET_NAME = var.LOGGING_BUCKET_NAME
+      LOGGING_BUCKET_KEY = var.LOGGING_BUCKET_KEY
+      LANDING_BUCKET = var.LANDING_BUCKET
+      YOUTUBE_BUCKET_KEY = var.YOUTUBE_BUCKET_KEY
+    }
+  }
+
+  timeout = 900 #Adjust the timeout of the function IN SECONDS
+  memory_size = 512 #Adjust the Memory of the function
+
+}
+
+resource "aws_lambda_function_url" "lambda_url" {
+  function_name = aws_lambda_function.api_lambda.function_name #Gives the AWS Lambda function a URL for us to trigger
+  authorization_type = "NONE"
+}
+
+# Grant permission for youtube-job-verification to invoke youtube-service-3
+resource "aws_lambda_permission" "allow_invocation_from_youtube_job_verification" {
+  statement_id  = "AllowInvocationFromYoutubeJobVerification"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "lambda.amazonaws.com"
+  source_arn    = data.aws_iam_role.existing_lambda_role.arn # ARN of the IAM role of youtube-job-verification
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "region" {
+    description = "Desired Region"
+    type = string
+    default = "us-east-2"
+}
+
+variable "LOGGING_BUCKET_NAME" {
+  description = "LOGGING_BUCKET_NAME"
+  type = string
+}
+variable "LOGGING_BUCKET_KEY" {
+  description = "LOGGING_BUCKET_KEY"
+  type = string
+}
+
+variable "LANDING_BUCKET" {
+  description = "LANDING_BUCKET"
+  type = string
+}
+
+variable "YOUTUBE_BUCKET_KEY" {
+  description = "YOUTUBE_BUCKET_KEY"
+  type = string
+}
+
+
+
+
